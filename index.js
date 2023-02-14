953ce97b8be761389002d8a6615ff0a2d796859e
Rozvita Lilly Štiglinc <117945803+19Lilly@users.noreply.github.com>
2023-02-14 09:14:28 +0100

    a

--- index.js
+++ index.js
@@ -1,45 +1,26 @@
 
-
-
 let homeScoreEl = document.getElementById("home-score");
 let guestScoreEl = document.getElementById("guest-score");
 
 let homeScore = 0; 
 let guestScore = 0;
 
-function add1Home() {
-    homeScore += 1;
-    homeScoreEl.textContent = homeScore;
+function addPoints(team,score) {
+    if (team == 'home') {
+        homeScore += score
+        homeScoreEl.textContent = homeScore
+    }
+    else {
+        guestScore += score
+        guestScoreEl.textContent = guestScore
+    }
 }
 
-function add2Home() {
-    homeScore += 2;
-    homeScoreEl.textContent = homeScore;
-}
-
-function add3Home() {
-    homeScore += 3;
-    homeScoreEl.textContent = homeScore;
-}
-
-function add1Guest() {
-    guestScore +=1;
-    guestScoreEl.textContent = guestScore;
-}
-
-function add1Guest() {
-    guestScore +=1;
-    guestScoreEl.textContent = guestScore;
-}
-
-function add2Guest() {
-    guestScore +=2;
-    guestScoreEl.textContent = guestScore;
-}
-
-function add3Guest() {
-    guestScore +=3;
-    guestScoreEl.textContent = guestScore;
+function newGame () {
+    homeScore = 0
+    guestScore = 0
+    homeScoreEl.textContent = homeScore
+    guestScoreEl.textContent = guestScore
 }
 
 
