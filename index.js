3180f68afdb6422f8be9bab0a6ebebeb7ffca73c
Rozvita Lilly Štiglinc <117945803+19Lilly@users.noreply.github.com>
2023-02-21 11:05:47 +0100

    zaoblene a zablokovane tlacitka, funkcia kto vyhrava

--- index.js
+++ index.js
@@ -9,21 +9,24 @@ let guestScore = 0;
 var timerId;
 var min = 4;
 var sec = 59;
-var isplay = true;
+var isplay = false;
 
 function addPoints(team,score) {
-    if (team == 'home') {
+    whoIsWinning()
+    if (team == 'home' && isplay == true) {
         homeScore += score
         homeScoreEl.textContent = homeScore
     }
-    else {
+    else if (team =='guest' && isplay == true){
         guestScore += score
         guestScoreEl.textContent = guestScore
     }
+    whoIsWinning()
 }
 
                     document.getElementById('pause').style.display = 'none';
                     function startTimer() {
+                    isplay = true;
                     var btton = document.getElementById('start').style.display = 'none';
                     document.getElementById('pause').style.display = 'inline';
                     
@@ -36,7 +39,7 @@ function addPoints(team,score) {
                         }
                         if (min == -1) {
                             clearTimeout(timerId);
-                            isplay=false;
+                            isplay = false;
                         }
                         if (sec < 10) {
                             sec = "0" + sec;
@@ -47,6 +50,7 @@ function addPoints(team,score) {
 
 function pause() {
   clearInterval(timerId);
+  isplay = false;
   document.getElementById('start').style.display = 'inline';
   document.getElementById('pause').style.display = 'none';
 }
@@ -57,8 +61,21 @@ function newGame () {
   guestScore = 0
   min = 4
   sec = 59
+  isplay = false;
   homeScoreEl.textContent = homeScore
   guestScoreEl.textContent = guestScore
+  document.getElementById("home-score").style.backgroundColor = "black";
+  document.getElementById("guest-score").style.backgroundColor = "black";
   timerEl.textContent = "5:00"
-  isplay = false;
+}
+
+function whoIsWinning() {
+    if (homeScore > guestScore) {
+        document.getElementById("home-score").style.backgroundColor = "#31F503";
+    } else if (guestScore > homeScore){
+        document.getElementById("guest-score").style.backgroundColor = "#31F503";
+    } else {
+        document.getElementById("home-score").style.backgroundColor = "black";
+        document.getElementById("guest-score").style.backgroundColor = "black";
+    }
 }
