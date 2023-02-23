cb190d971de8ea12d1897681f476b3baa82b823d
Rozvita Lilly Štiglinc <117945803+19Lilly@users.noreply.github.com>
2023-02-24 00:36:57 +0100

    finalny dizajn snad + vymazanie nepotrebneho obrazku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 let homeScoreEl = document.getElementById("home-score");
 let guestScoreEl = document.getElementById("guest-score");
 let timerEl = document.getElementById("timer");
-document.getElementById('new-game-btn').disabled=true;
+document.getElementById("new-game-btn").disabled=true;
 
 let homeScore = 0; 
 let guestScore = 0;
@@ -15,50 +15,50 @@ var isplay = false;
 
 function addPoints(team,score) {
     
-    if (team === 'home' && isplay === true) {
+    if (team === "home" && isplay === true) {
         homeScore += score
         homeScoreEl.textContent = homeScore
     }
-    else if (team ==='guest' && isplay === true){
+    else if (team ==="guest" && isplay === true){
         guestScore += score
         guestScoreEl.textContent = guestScore
     }
     whoIsWinning()
 }
 
-                    document.getElementById('pause').style.display = 'none';
+                    document.getElementById("pause").style.display = "none";
                     function startTimer() {
-                    isplay = true;
-                    document.getElementById('new-game-btn').disabled=true;
-                    var btton = document.getElementById('start').style.display = 'none';
-                    document.getElementById('pause').style.display = 'inline';
-                    
-                    timerId = setInterval(function () {
-                        document.getElementById('timer').textContent = min + ":" + sec;
-                        sec--;
-                        if (sec == -1) {
-                            min--;
-                            sec = 59;
-                        }
-                        if (min == -1) {
-                            clearTimeout(timerId);
-                            isplay = false;
-                        }
-                        if (sec < 10) {
-                            sec = "0" + sec;
-                        }
-                    }, 1000);
-                   
-
-                }
+                            isplay = true;
+                            document.getElementById("new-game-btn").disabled=true;
+                            var btton = document.getElementById("start").style.display = "none";
+                            document.getElementById("pause").style.display = "inline";
+                            
+                            timerId = setInterval(function () {
+                                document.getElementById("timer").textContent = min + ":" + sec;
+                                sec--;
+                                if (sec == -1) {
+                                    min--;
+                                    sec = 59;
+                                }
+                                if (min == -1) {
+                                    clearTimeout(timerId);
+                                    isplay = false;
+                                }
+                                if (sec < 10) {
+                                    sec = "0" + sec;
+                                }
+                                if (min == 0 && sec == 0){
+                                    document.getElementById("new-game-btn").disabled=false;
+                                }
+                            }, 1000);
+                    }
 
 function pause() {
   clearInterval(timerId);
   isplay = false;
-  document.getElementById('new-game-btn').disabled=false;
-  document.getElementById('start').style.display = 'inline';
-  document.getElementById('pause').style.display = 'none';
-  
+  document.getElementById("new-game-btn").disabled=false;
+  document.getElementById("start").style.display = "inline";
+  document.getElementById("pause").style.display = "none";
 }
 
 
@@ -73,14 +73,16 @@ function newGame () {
   document.getElementById("home-score").style.backgroundColor = "black";
   document.getElementById("guest-score").style.backgroundColor = "black";
   timerEl.textContent = "5:00"
+  document.getElementById("start").style.display = "inline";
+  document.getElementById("pause").style.display = "none";
 }
 
 function whoIsWinning() {
     if (homeScore > guestScore) {
-        document.getElementById("home-score").style.backgroundColor = "#31F503";
+        document.getElementById("home-score").style.backgroundColor = "#5efd52";
         document.getElementById("guest-score").style.backgroundColor = "black";
     } else if (guestScore > homeScore){
-        document.getElementById("guest-score").style.backgroundColor = "#31F503";
+        document.getElementById("guest-score").style.backgroundColor = "#5efd52";
         document.getElementById("home-score").style.backgroundColor = "black";
     } else {
         document.getElementById("home-score").style.backgroundColor = "black";
